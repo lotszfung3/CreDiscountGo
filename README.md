105a77a0d6a5cc97ba3d0696b6ca9046d4d75876
lotszfung3 <lotszfung3@gmail.com>
2018-07-07 09:40:17 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# CreDiscountGo
+A demo mobile application
+
+# Notes
+## Features
+1. Google map
+2. Button on the bottom with callback
+3. Useless Search Bar on the top
+4. Few Markers will be shown after pressing the leftest button
+
+## Ways to use
+1. Clone this repo
+2. Import it using Android Studio
---
+++ app/src/main/java/com/example/crediscountgo/MainActivity.java
@@ -0,0 +1,211 @@
+package com.example.crediscountgo;
+
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,OnMapReadyCallback {
+    private GoogleMap mMap;
+    private double markerCoor[] = new double[20];
+    private ArrayList<Circle> circleArrayList;
+    private FloatingActionButton floatingActionButton;
+    LatLng KT = new LatLng(22.3088477, 114.217971);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        setUpMap();
+
+        setUpFloatingBtn();
+
+
+    }
+
+    private void setUpFloatingBtn() {
+        floatingActionButton = (FloatingActionButton)findViewById(R.id.floatingBtn1);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Creating the instance of PopupMenu
+                PopupMenu popup = new PopupMenu(MainActivity.this, floatingActionButton);
+                //Inflating the Popup using xml file
+                popup.getMenuInflater()
+                        .inflate(R.menu.activity_main_drawer, popup.getMenu());
+
+                //registering popup with OnMenuItemClickListener
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(
+                                MainActivity.this,
+                                "You Clicked : " + item.getTitle(),
+                                Toast.LENGTH_SHORT
+                        ).show();
+                        return true;
+                    }
+                });
+
+                popup.show(); //showing popup menu
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    private void setUpMap(){
+        Random rand= new Random();
+        for (int i=0;i < 10 ;i++)
+        {
+
+            markerCoor[2*i]= KT.latitude + rand.nextGaussian()/1000;
+            markerCoor[2*i+1]= KT.longitude + rand.nextGaussian()/1000;
+        }
+        circleArrayList = new ArrayList<>(10);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.style_json));
+
+            if (!success) {
+                Log.e("fra", "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e("fra", "Can't find style. Error: ", e);
+        }
+        // Add a marker in Sydney and move the camera
+        LatLng HK = new LatLng(22.2829369,114.1828038);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(HK,10));
+
+        // Add circle
+        for(int i=0;i<markerCoor.length/2;i++) {
+
+            CircleOptions circleOptions = new CircleOptions().center(new LatLng(markerCoor[2*i],markerCoor[2*i+1])).radius(30).strokeWidth(3).fillColor(Color.argb(128, 255, 0, 0));
+            circleArrayList.add(mMap.addCircle(circleOptions));
+        }
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                initCameraPosition();
+            }
+        }, 1500);
+
+    }
+    private void initCameraPosition()
+    {
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(KT)      // Sets the center of the map to Mountain View
+                .zoom(16.5f)                   // Sets the zoom
+                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition),2000,null);
+    }
+}
