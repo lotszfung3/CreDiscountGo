ee2338cbd282b8afc039b5c02abbc5f13bbb38b0
choiwaiyiu <benchoi@fundroots.com>
2018-07-07 21:03:51 +0800

    profile credit card view

--- app/src/main/java/com/example/crediscountgo/UserProfile.java
+++ app/src/main/java/com/example/crediscountgo/UserProfile.java
@@ -2,6 +2,11 @@ package com.example.crediscountgo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserProfile extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class UserProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        List<Integer> creditCardIdList = new ArrayList<>();
+        creditCardIdList.add(new Integer(R.drawable.full_logo));
+        creditCardIdList.add(new Integer(R.drawable.full_logo));
+        creditCardIdList.add(new Integer(R.drawable.full_logo));
+        creditCardIdList.add(new Integer(R.drawable.full_logo));
+        creditCardIdList.add(new Integer(R.drawable.full_logo));
+
+
+        RecyclerView creditCardRecyclerView = (RecyclerView) findViewById(R.id.credit_card_recycler_view);
+        CreditCardAdapter creditCardAdapter = new CreditCardAdapter(creditCardIdList);
+        creditCardRecyclerView.setAdapter(creditCardAdapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        creditCardRecyclerView.setLayoutManager(layoutManager);
     }
 }
