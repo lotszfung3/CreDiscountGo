2c7f8b5d9b220197b2d6cd57a844ea5510eaaf45
oliviaolivia700 <olivialai7@gmail.com>
2018-07-07 23:19:34 +0800

    finishMarkerInfo

--- app/src/main/java/com/example/crediscountgo/Card.java
+++ app/src/main/java/com/example/crediscountgo/Card.java
@@ -2,5 +2,19 @@ package com.example.crediscountgo;
 
 public class Card {
     String initials;
+    String iconURL;
+
+    public Card(String i, String u){
+        initials = i;
+        iconURL = u;
+    }
+
+    public String getInitials(){
+        return initials;
+    }
+
+    public String getIconURL(){
+        return iconURL;
+    }
 
 }
